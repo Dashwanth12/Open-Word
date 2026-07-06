679990643d8f2ffb815e9a618368417409baee8b
dashwanth <dashwanth@dashwanths-Air.lan>
2026-07-06 15:48:57 +0530

    add custom domain to cors

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,9 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     origin: [
-        'http://localhost:5173',
-        'https://open-word-chi.vercel.app'  // ← add this
+        'https://open-word-chi.vercel.app',
+        'https://openwordbooks.com',
+        'https://www.openwordbooks.com'
     ],
     credentials: true
 }))
